cc7a082dc2e70fc12a3e3e245db5ead261589b8f
unknown <hcozagar@gmail.com>
2022-04-19 15:37:07 +0300

    sec

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"https://api.unsplash.com",
-    headers:{
-        Authorization: "Client-ID LuFkowtmQrUmi2FxQr4UNRBgYnVrqbNvZITdjeSK6iU"
-        }
-})
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization: "" /*CLIENT ID & API_KEY */,
+  },
+});
